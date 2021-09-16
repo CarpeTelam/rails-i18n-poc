50c7cc6d0e433f6a5538660ce69cb4892ca5ceb4
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2021-09-16 21:50:12 +0000

    Setup locale GET param

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController < ApplicationController
+    before_action :set_locale
+
     def index; end
+
+    private
+
+    def set_locale
+        I18n.locale = params[:locale] || I18n.default_locale
+    end
 end
