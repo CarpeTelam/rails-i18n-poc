7e059c2166dc403766d937c68c2d21884f6621ca
Jeff Huey Huelsbeck <huey@carpetelam.com>
2021-09-16 23:32:49 +0000

    Add ability to switch locales

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,3 @@
 class PagesController < ApplicationController
-    before_action :set_locale
-
     def index; end
-
-    private
-
-    def set_locale
-        I18n.locale = params[:locale] || I18n.default_locale
-    end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module RailsI18nPoc
     config.load_defaults 5.2
 
     config.i18n.available_locales = [:en, :es]
-    config.i18n.default_locale = :es
+    config.i18n.default_locale = :en
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
